e692cea214f976f0289af04348c1d16f9731db6b
TheRangiCrew <ryan.z.adam@gmail.com>
2025-08-21 23:42:51 +1200

    Improve functions and add tests for TML

---
+++ pkg/awips/tml.go
@@ -0,0 +1,104 @@
+package awips
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/twpayne/go-geom"
+)
+
+// The rules of TIME...MOT...LOC information in text products is defined in NWS directive 10-1701 section 5.7.
+
+const TMLRegexp = `(?m:^(TIME\.\.\.MOT\.\.\.LOC)([A-Za-z0-9 \n]*))`
+
+type TML struct {
+	Original    string        `json:"original"`
+	Time        time.Time     `json:"time"`
+	Direction   int           `json:"direction"`
+	Speed       int           `json:"speed"`
+	SpeedString string        `json:"speedString"`
+	Locations   []*geom.Point `json:"location"`
+}
+
+// Find the TIME...MOT...LOC information in the text.
+func FindTML(text string) string {
+	tmlRegexp := regexp.MustCompile(TMLRegexp)
+	return strings.TrimSpace(tmlRegexp.FindString(text))
+}
+
+// Attempts to find the TIME...MOT...LOC information in the text and parse it.
+// If the string is not found, it returns nil.
+// If the string is found but cannot be parsed, it returns an error.
+func ParseTML(text string, issued time.Time) (*TML, error) {
+
+	original := FindTML(text)
+	if original == "" {
+		return nil, nil
+	}
+
+	trimRegexp := regexp.MustCompile(`[\s\n]+`)
+	original = trimRegexp.ReplaceAllString(original, " ")
+	fmt.Println(original)
+
+	// Split the string into segments. Segments are separated by spaces.
+	segments := strings.Split(original, " ")[1:]
+
+	// Parse the time
+	parsedTime, err := time.Parse(("1504Z"), segments[0])
+	if err != nil {
+		return nil, errors.New("could not parse TML time: " + err.Error())
+	}
+
+	time := time.Date(issued.Year(), issued.Month(), issued.Day(), parsedTime.Hour(), parsedTime.Minute(), 0, 0, time.Now().UTC().Location())
+
+	// Parse the direction
+	direction, err := strconv.Atoi(segments[1][:3])
+	if err != nil {
+		return nil, errors.New("could not parse direction in TML: " + err.Error())
+	}
+
+	numberRegexp := regexp.MustCompile("[0-9]+")
+
+	// Parse the speed
+	// The directive says the speed is given as knots. However, some products use miles per hour.
+	speedString := segments[2]
+	speed, err := strconv.Atoi(numberRegexp.FindString(speedString))
+	if err != nil {
+		return nil, errors.New("could not parse speed in TML: " + err.Error())
+	}
+
+	points := []*geom.Point{}
+
+	// Parse the location(s).
+	// TML information can contain multiple pairs of latitude and longitude information whether the product is talking about an event that is isolated to a single point or the event is occurring along a line
+	for i := 3; i < len(segments)-1; i += 2 {
+		lat, err := Parse45Digit(segments[i])
+		if err != nil {
+			return nil, errors.New("could not parse latitude in TML: " + err.Error())
+		}
+		lon, err := Parse45Digit(segments[i+1])
+		if err != nil {
+			return nil, errors.New("could not parse longitude in TML: " + err.Error())
+		}
+		p, err := geom.NewPoint(geom.XY).SetCoords([]float64{float64(lon), float64(lat)})
+		if err != nil {
+			return nil, errors.New("could not create point in TML: " + err.Error())
+		}
+		points = append(points, p)
+	}
+
+	tml := TML{
+		Original:    original,
+		Time:        time,
+		Direction:   direction,
+		Speed:       speed,
+		SpeedString: speedString,
+		Locations:   points,
+	}
+
+	return &tml, nil
+}
---
+++ pkg/awips/tml_test.go
@@ -0,0 +1,96 @@
+package awips
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindTML(t *testing.T) {
+	// Example from NWS Directive 10-1701
+	text := `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318`
+	tml := FindTML(text)
+	assert.NotEqual(t, "", tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318", tml)
+
+	// Multiple points
+	text = `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 3490 10420`
+	tml = FindTML(text)
+	assert.NotEqual(t, "", tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 3490 10420", tml)
+
+	// Multiple lines
+	text = `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 
+    3490 10420`
+	tml = FindTML(text)
+	assert.NotEqual(t, "", tml)
+	assert.Equal(t, `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 
+    3490 10420`, tml)
+
+	// Lowercase prefix (should not match)
+	text = `time...mot...loc 0128Z 004DEG 9KT 3480 10318`
+	tml = FindTML(text)
+	assert.Equal(t, "", tml)
+}
+
+func TestParseTML(t *testing.T) {
+	issued := time.Date(2025, 8, 21, 0, 0, 0, 0, time.UTC)
+
+	// Valid TML
+	text := `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318`
+	tml, err := ParseTML(text, issued)
+	assert.NoError(t, err)
+	assert.NotNil(t, tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318", tml.Original)
+	assert.Equal(t, time.Date(2025, 8, 21, 1, 28, 0, 0, time.UTC), tml.Time)
+	assert.Equal(t, 4, tml.Direction)
+	assert.Equal(t, 9, tml.Speed)
+	assert.Equal(t, "9KT", tml.SpeedString)
+	assert.Equal(t, 1, len(tml.Locations))
+	assert.Equal(t, 103.18, tml.Locations[0].X())
+	assert.Equal(t, 34.8, tml.Locations[0].Y())
+
+	// Multiple locations
+	text = `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 3490 10420`
+	tml, err = ParseTML(text, issued)
+	assert.NoError(t, err)
+	assert.NotNil(t, tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 3490 10420", tml.Original)
+	assert.Equal(t, 2, len(tml.Locations))
+	assert.Equal(t, 104.2, tml.Locations[1].X())
+	assert.Equal(t, 34.9, tml.Locations[1].Y())
+
+	// Multi Line TML
+	text = `TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318
+    3490 10420`
+	tml, err = ParseTML(text, issued)
+	assert.NoError(t, err)
+	assert.NotNil(t, tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9KT 3480 10318 3490 10420", tml.Original)
+	assert.Equal(t, 2, len(tml.Locations))
+	assert.Equal(t, 103.18, tml.Locations[0].X())
+	assert.Equal(t, 34.8, tml.Locations[0].Y())
+	assert.Equal(t, 2, len(tml.Locations))
+	assert.Equal(t, 104.2, tml.Locations[1].X())
+	assert.Equal(t, 34.9, tml.Locations[1].Y())
+
+	// Motion less than 1
+	text = `TIME...MOT...LOC 0128Z 004DEG 0KT 3480 10318`
+	tml, err = ParseTML(text, issued)
+	assert.NoError(t, err)
+	assert.NotNil(t, tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 0KT 3480 10318", tml.Original)
+	assert.Equal(t, 0, tml.Speed) // Speed should be 0
+
+	// Some products have the motion in a unit other than knots. Test for this.
+	text = `TIME...MOT...LOC 0128Z 004DEG 9MPH 3480 10318`
+	tml, err = ParseTML(text, issued)
+	assert.NoError(t, err)
+	assert.NotNil(t, tml)
+	assert.Equal(t, "TIME...MOT...LOC 0128Z 004DEG 9MPH 3480 10318", tml.Original)
+	assert.Equal(t, 9, tml.Speed)            // Speed should still be parsed as knots
+	assert.Equal(t, "9MPH", tml.SpeedString) // Speed string should reflect
+}
+
+// text := `TIME...MOT...LOC 1300Z 090DEG 20KT 3881 10015`
